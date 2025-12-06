77e45260020bd690be0eb04e7837e666795fa0b6
Thanh Quân <phunglethanhquan1232005@gmail.com>
2025-12-06 21:18:00 +0700

    Overwrite entire project

---
+++ src/main/java/vn/cococord/CococordApplication.java
@@ -0,0 +1,13 @@
+package vn.cococord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CococordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CococordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/cococord/CococordApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.cococord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CococordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
